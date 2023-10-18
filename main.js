6957007cfee63d64bfadd4dd381796e900436229
Giancarlo0811 <giancarlo.t0811@gmail.com>
2023-10-18 15:48:18 -0400

    errores agregados

--- main.js
+++ main.js
@@ -7,38 +7,42 @@ const weatherContainer = document.querySelector('.weather-container');
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`,{mode: 'cors'});
-    const data = await response.json();
+    if (response.status == 404 || response.status == 400) {
+        document.querySelector('.error').style.display = 'block';
+        weatherContainer.style.display = 'none';
+    }
+    else {
+        const data = await response.json();
 
-    document.querySelector('.celsius').textContent = `${Math.round(data.main.temp)}°C`;
-    document.querySelector('.city').textContent = data.name;
-    document.querySelector('.humidity-percentage').textContent = `${data.main.humidity}%`;
-    document.querySelector('.speed').textContent = `${data.wind.speed} km/h`;
-    weatherContainer.style.display = 'flex';
+        document.querySelector('.celsius').textContent = `${Math.round(data.main.temp)}°C`;
+        document.querySelector('.city').textContent = data.name;
+        document.querySelector('.humidity-percentage').textContent = `${data.main.humidity}%`;
+        document.querySelector('.speed').textContent = `${data.wind.speed} km/h`;
 
-    if(data.weather[0].main == 'Clouds') {
+        if(data.weather[0].main == 'Clouds') {
         weatherIcon.src = './images/clouds.png';
-    } 
-    else if (data.weather[0].main == 'Clear') {
+        } 
+        else if (data.weather[0].main == 'Clear') {
         weatherIcon.src = './images/clear.png';
-    } 
-    else if (data.weather[0].main == 'Rain') {
-        weatherIcon.src = './images/rain.png';
-    } 
-    else if (data.weather[0].main == 'Drizzle') {
-        weatherIcon.src = './images/drizzle.png';
-    } 
-    else if (data.weather[0].main == 'Mist') {
-        weatherIcon.src = './images/mist.png';
+        } 
+        else if (data.weather[0].main == 'Rain') {
+            weatherIcon.src = './images/rain.png';
+        } 
+        else if (data.weather[0].main == 'Drizzle') {
+            weatherIcon.src = './images/drizzle.png';
+        } 
+        else if (data.weather[0].main == 'Mist') {
+            weatherIcon.src = './images/mist.png';
+        }
+
+        weatherContainer.style.display = 'flex';
+        document.querySelector('.error').style.display = 'none';
     }
 }
 
 
 
 searchBtn.addEventListener('click', () => {
-    if (inputCity.value == '') {
-        alert('Debes introducir el nombre de una ciudad');
-    } else {
-        checkWeather(inputCity.value);
-        inputCity.value = '';
-    }
+    checkWeather(inputCity.value);
+    inputCity.value = '';
 });
